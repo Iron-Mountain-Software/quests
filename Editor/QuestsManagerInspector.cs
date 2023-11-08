295d101b98029dbaefb95676e91308e99a17b182
ludachrismonte <ludachrismonte@gmail.com>
2023-11-08 09:21:18 -0500

    manager inspector

---
+++ Editor/QuestsManagerInspector.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+
+namespace IronMountain.Quests.Editor
+{
+    [CustomEditor(typeof(QuestsManager), true)]
+    public class SceneManagerInspector : UnityEditor.Editor
+    {
+        private UnityEditor.Editor _databaseInspector;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            Database database = serializedObject.FindProperty("database").objectReferenceValue as Database;
+            if (database)
+            {
+                CreateCachedEditor(database, null, ref _databaseInspector);
+                _databaseInspector.OnInspectorGUI();
+            }
+            else _databaseInspector = null;
+        }
+    }
+}
